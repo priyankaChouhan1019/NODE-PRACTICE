f93a8f411012a654547645c93336681b715f3489
priyankaChouhan1019 <priyachouhan1019@gmail.com>
2023-11-26 23:27:00 +0530

    route-level- middleware

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 const express = require('express');
 const app = express();
+const reqFilter = require('./middleware')
+//2nd way
+const route = express.Router();
+route.use(reqFilter);
 
-// below reqFilter accepts three parameters
-const reqFilter = (req, resp, next)=>{
-   if(!req.query.age){
-      resp.send("Please provide age")
-   } else if (req.query.age < 18){
-      resp.send("you are under age")
-   } else{
-      next();
-   }
-}
+// if we don't have to apply route do we will use app.get , otherwise rotue.get
 
-   app.use(reqFilter);
+//1st way
+//app.use(reqFilter);
 
    app.get('/', (req, resp)=>{
       resp.send("Welcome to home page")
 
    })
+ // route level middleware
+ //1st way  
+   // app.get('/user',reqFilter, (req, resp)=>{
+   //    resp.send("Welcome to user page")
+   // })
 
-   app.get('/user', (req, resp)=>{
-      resp.send("Welcome to user page")
+   //2nd way
+   route.get('/user', (req, resp)=>{
+          resp.send("Welcome to user page")
+    })
+
+   app.get('/about', (req, resp)=>{
+      resp.send("Welcome to about page")
    })
+
+   //2nd way
+   app.use('/', route);
 
    app.listen(5000)
 
---
+++ middleware.js
@@ -0,0 +1,10 @@
+// below reqFilter accepts three parameters
+module.exports = reqFilter = (req, resp, next)=>{
+    if(!req.query.age){
+       resp.send("Please provide age")
+    } else if (req.query.age < 18){
+       resp.send("you are under age")
+    } else{
+       next();
+    }
+ }
